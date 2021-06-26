a240df97238fc718b9b5d8568adb86405070266d
Chachchai Tongthavornsuwan <chachchai.toon@gmail.com>
2021-06-26 21:08:17 +0700

    response html file

--- section-5_ExpressJS/app.js
+++ section-5_ExpressJS/app.js
@@ -1,29 +1,20 @@
+const path = require("path");
+
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use("/", (req, res, next) => {
-  console.log("This always run!");
-  next();
-});
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
-app.use("/product", (req, res, next) => {});
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/add-product", (req, res, next) => {
-  console.log("In another middleware!");
-  res.send(`<html>
-    <body>
-      <form action="/product" method="POST">
-        <input type="text" name="title">
-        <button type="submit">Add Product</button>
-      </form>
-    </body>
-  </html>`);
-});
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
 
-app.use("/", (req, res, next) => {
-  console.log("In another middleware!");
-  res.send("<h1>Hello from express</h1>");
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(3000);
