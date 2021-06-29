fb5d6f4f4050c6daff7f18ca00fa5bee4c85790a
Chachchai Tongthavornsuwan <chachchai.toon@gmail.com>
2021-06-29 22:48:49 +0700

    learn handlebars

--- section-6_Dynamic-content-Template-engine/app.js
+++ section-6_Dynamic-content-Template-engine/app.js
@@ -2,11 +2,18 @@ const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const expressHbs = require("express-handlebars");
 
 const app = express();
 
-app.set("view engine", "pug");
+/* View Engine -> Handlebars template */
+app.engine("hbs", expressHbs());
+app.set("view engine", "hbs");
 app.set("views", "views");
+
+/* View Engine -> PUG template */
+// app.set("view engine", "pug");
+// app.set("views", "views");
 
 const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
@@ -18,7 +25,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  res.status(404).render("404", { pageTitle: "Page Not Found" });
 });
 
 app.listen(3000);
