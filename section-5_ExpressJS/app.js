e2b8d9c1f02b1cd620beba6b5b298a859446b69d
Chachchai Tongthavornsuwan <chachchai.toon@gmail.com>
2021-06-29 08:55:57 +0700

    Finished expressjs basic

--- section-5_ExpressJS/app.js
+++ section-5_ExpressJS/app.js
@@ -1,7 +1,6 @@
 const path = require("path");
 
 const express = require("express");
-const bodyParser = require("body-parser");
 
 const app = express();
 
@@ -9,10 +8,11 @@ const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
+// All routes
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
-
 app.use((req, res, next) => {
   res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
