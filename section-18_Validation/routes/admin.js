0d7207f51b8dfaad0758e3380c1ecdf34f642314
Chachchai Tongthavornsuwan <chachchai.toon@gmail.com>
2021-07-26 21:37:44 +0700

    Finished section 18 - Validation

--- section-18_Validation/routes/admin.js
+++ section-18_Validation/routes/admin.js
@@ -1,41 +1,56 @@
-const express = require("express");
+const path = require('path');
 
-const { body } = require("express-validator/check");
+const express = require('express');
+const { body } = require('express-validator/check');
 
-const adminController = require("../controllers/admin");
-const isAuth = require("../middleware/is-auth");
+const adminController = require('../controllers/admin');
+const isAuth = require('../middleware/is-auth');
 
 const router = express.Router();
 
 // /admin/add-product => GET
-router.get("/add-product", isAuth, adminController.getAddProduct);
+router.get('/add-product', isAuth, adminController.getAddProduct);
 
 // /admin/products => GET
-router.get("/products", isAuth, adminController.getProducts);
+router.get('/products', isAuth, adminController.getProducts);
 
 // /admin/add-product => POST
 router.post(
-  "/add-product",
+  '/add-product',
   [
-    body("title", "Title must have at least 1 character!").not().isEmpty(),
-    body("imageUrl")
-      .not()
-      .isEmpty()
-      .withMessage("Image is required!")
-      .isURL()
-      .withMessage("Image must be only URL!"),
-    body("price")
-      .isFloat({ min: 0.05, max: 99.99 })
-      .withMessage("Price must be decimal value with range 0.05$ to 99.99$!"),
+    body('title')
+      .isString()
+      .isLength({ min: 3 })
+      .trim(),
+    body('imageUrl').isURL(),
+    body('price').isFloat(),
+    body('description')
+      .isLength({ min: 5, max: 400 })
+      .trim()
   ],
   isAuth,
   adminController.postAddProduct
 );
 
-router.get("/edit-product/:productId", isAuth, adminController.getEditProduct);
+router.get('/edit-product/:productId', isAuth, adminController.getEditProduct);
 
-router.post("/edit-product", isAuth, adminController.postEditProduct);
+router.post(
+  '/edit-product',
+  [
+    body('title')
+      .isString()
+      .isLength({ min: 3 })
+      .trim(),
+    body('imageUrl').isURL(),
+    body('price').isFloat(),
+    body('description')
+      .isLength({ min: 5, max: 400 })
+      .trim()
+  ],
+  isAuth,
+  adminController.postEditProduct
+);
 
-router.post("/delete-product", isAuth, adminController.postDeleteProduct);
+router.post('/delete-product', isAuth, adminController.postDeleteProduct);
 
 module.exports = router;
