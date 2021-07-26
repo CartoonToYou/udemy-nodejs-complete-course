8e434e1f3f7deb4a9f5161fe03d72f9ad4f2b72e
Chachchai Tongthavornsuwan <chachchai.toon@gmail.com>
2021-07-26 21:57:27 +0700

    Start error handling

---
+++ section-19_Error-Handling/routes/auth.js
@@ -0,0 +1,84 @@
+const express = require("express");
+const { check, body } = require("express-validator/check");
+
+const authController = require("../controllers/auth");
+const User = require("../models/user");
+
+const router = express.Router();
+
+router.get("/login", authController.getLogin);
+
+router.get("/signup", authController.getSignup);
+
+router.post(
+  "/login",
+  [
+    body("email")
+      .isEmail()
+      .withMessage("Please enter a valid email.")
+      .normalizeEmail(),
+    body(
+      "password",
+      "Please enter a password with only numbers and text and at least 5 characters"
+    )
+      .isLength({ min: 5 })
+      .isAlphanumeric()
+      .trim(),
+  ],
+  authController.postLogin
+);
+
+router.post(
+  "/signup",
+  /* use array for many fields to validate */
+  [
+    /* withMessage will show error after method that it chained */
+    check("email")
+      .isEmail()
+      .withMessage("Please enter a valid email.")
+      .custom((value, { req }) => {
+        // if (value === "test@test.com") {
+        //   throw new Error("This is email address is forbidden.");
+        // }
+        // return true;
+        /* Async validation */
+        return User.findOne({ email: value }).then((userDoc) => {
+          if (userDoc) {
+            return Promise.reject(
+              "E-Mail exists already, please pick a different one."
+            );
+          }
+        });
+      })
+      .normalizeEmail(),
+    /* 2nd argument in check, body or others will show error message in all validation condition (default message) */
+    body(
+      "password",
+      "Please enter a password with only numbers and text and at least 5 characters"
+    )
+      .isLength({ min: 5 })
+      .isAlphanumeric()
+      .trim(),
+    body("confirmPassword")
+      .trim()
+      .custom((value, { req }) => {
+        if (value !== req.body.password) {
+          throw new Error("Passwords have to match!");
+        }
+        return true;
+      }),
+  ],
+  authController.postSignup
+);
+
+router.post("/logout", authController.postLogout);
+
+router.get("/reset", authController.getReset);
+
+router.post("/reset", authController.postReset);
+
+router.get("/reset/:token", authController.getNewPassword);
+
+router.post("/new-password", authController.postNewPassword);
+
+module.exports = router;
