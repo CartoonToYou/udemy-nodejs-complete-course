ca4dd320b10cfcd2aa0eb9f343a6dbd1a4a26efe
Chachchai Tongthavornsuwan <chachchai.toon@gmail.com>
2021-07-27 21:28:50 +0700

    using input type=file

--- section-20_FileUploadDownload/app.js
+++ section-20_FileUploadDownload/app.js
@@ -85,6 +85,7 @@ app.get("/500", errorController.get500);
 app.use(errorController.get404);
 
 app.use((error, req, res, next) => {
+  console.log(req.session);
   // res.status(error.httpStatusCode).render(...);
   // res.redirect('/500');
   res.status(500).render("500", {
