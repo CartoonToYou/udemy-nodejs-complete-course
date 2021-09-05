53d7b224cf9eea7834d8c6ea06dff1ea26921cee
Chachchai Tongthavornsuwan <89886557+Chachchai-Tong@users.noreply.github.com>
2021-09-05 15:03:33 +0700

    feat: done using ES module in NodeJS

--- section-32_ModernJSwithNodeJS/response-handler.js
+++ section-32_ModernJSwithNodeJS/response-handler.js
@@ -1,10 +1,23 @@
-// const fs = require("fs");
-import fs from "fs";
+// const fs = require("fs").promises; // Don't use ('fs/promises')
+/* Using Promises in Core NodeJS module instead of callback to avoid callback hell */
+import fs from "fs/promises";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
+/* Global variable => use for define path of this file name */
+const __filename = fileURLToPath(import.meta.url);
+/* Current folder that this file is exist */
+const __dirname = dirname(__filename);
 
 export const resHandler = (req, res, next) => {
-  fs.readFile("my-page.html", "utf8", (err, data) => {
-    res.send(data);
-  });
+  fs.readFile("my-page.html", "utf8")
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+  // res.sendFile(path.join(__dirname, "my-page.html"));
 };
 
 // module.exports = resHandler;
