ae9a3b96a5b81e6cf89ca27c72b50624d0c7413f
Chachchai Tongthavornsuwan <89886557+Chachchai-Tong@users.noreply.github.com>
2021-09-27 20:12:33 +0700

    feat: learn to start http server with deno

--- section-34_IntroToDeno/app.ts
+++ section-34_IntroToDeno/app.ts
@@ -1,8 +1,15 @@
-const text = 'This is a test - and it should be stored in a file!👌'
+// import { listenAndServe } from "https://deno.land/std@0.108.0/http/server.ts";
 
-const encoder = new TextEncoder()
-const data = encoder.encode(text)
+// const server = listenAndServe(':3000', () => new Response('Hello World\n'))
 
-Deno.writeFile('message.txt', data).then(() => {
-  console.log('Wrote to file!')
-})
+// console.log(server,"http://localhost:3000/")
+
+
+import { serve } from "https://deno.land/std@0.108.0/http/server_legacy.ts";
+
+const server = serve({ port: 8000 });
+console.log("http://localhost:8000/");
+
+for await (const req of server) {
+  req.respond({ body: "Hello World\n" });
+}
