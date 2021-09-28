34fd8eaa51a35369b41596ae7b977e9e1c4d2164
Chachchai Tongthavornsuwan <89886557+Chachchai-Tong@users.noreply.github.com>
2021-09-28 08:45:32 +0700

    feat: compare APIs between node and deno app

--- section-34_IntroToDeno/app.ts
+++ section-34_IntroToDeno/app.ts
@@ -1,15 +1,9 @@
-// import { listenAndServe } from "https://deno.land/std@0.108.0/http/server.ts";
+import { Application } from "https://deno.land/x/oak/mod.ts";
 
-// const server = listenAndServe(':3000', () => new Response('Hello World\n'))
+const app = new Application();
 
-// console.log(server,"http://localhost:3000/")
+app.use((ctx) => {
+  ctx.response.body = "Hello World!";
+});
 
-
-import { serve } from "https://deno.land/std@0.108.0/http/server_legacy.ts";
-
-const server = serve({ port: 8000 });
-console.log("http://localhost:8000/");
-
-for await (const req of server) {
-  req.respond({ body: "Hello World\n" });
-}
+await app.listen({ port: 8000 });
